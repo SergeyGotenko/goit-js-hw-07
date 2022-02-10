b01ca8cc07e63abe730e4b42c837322f401addbe
SergeyGotenko <90980253+SergeyGotenko@users.noreply.github.com>
2022-02-10 13:58:14 +0200

    debug

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,13 +4,13 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 const galleryContainer = document.querySelector('.gallery');
 
-const renderGalletyMarkup = galleryItems.map(({ preview, original, description }) =>
+const renderGalleryMarkup = galleryItems.map(({ preview, original, description }) =>
   `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a>`
 ).join('');
     
-galleryContainer.insertAdjacentHTML("beforeend", renderGalletyMarkup);
+galleryContainer.insertAdjacentHTML("beforeend", renderGalleryMarkup);
 
 
 
